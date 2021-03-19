5a80630252b58fd46a426df7d507550cf6ec68f2
Burak Baskın <2011404076@ogr.mehmetakif.edu.tr>
2021-03-19 23:46:50 +0300

    Fixed filtering by brand name in car filter.

--- src/app/pipes/car-filter.pipe.ts
+++ src/app/pipes/car-filter.pipe.ts
@@ -8,9 +8,8 @@ export class CarFilterPipe implements PipeTransform {
 
   transform(value: Car[], filterText: string): Car[] {
     filterText = filterText?filterText.toLocaleLowerCase():"";
-    let result = filterText?value.filter((c:Car)=>c.description.toLocaleLowerCase().indexOf(filterText)!==-1):value +
-    filterText?value.filter((c:Car)=>c.brandName.toLocaleLowerCase().indexOf(filterText)!==-1):value;
-    return result;
+    return filterText?value.
+    filter((c:Car)=>`${c.brandName} ${c.description}`.toLocaleLowerCase().indexOf(filterText)!==-1):value
   }
 
 }
