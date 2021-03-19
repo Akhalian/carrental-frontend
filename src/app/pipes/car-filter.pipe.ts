f15c9ba5e060de465cafd5968f1a2dfce26a29f5
Burak Baskın <2011404076@ogr.mehmetakif.edu.tr>
2021-03-19 23:54:13 +0300

    Added filtering by model year for car filter.

--- src/app/pipes/car-filter.pipe.ts
+++ src/app/pipes/car-filter.pipe.ts
@@ -9,7 +9,7 @@ export class CarFilterPipe implements PipeTransform {
   transform(value: Car[], filterText: string): Car[] {
     filterText = filterText?filterText.toLocaleLowerCase():"";
     return filterText?value.
-    filter((c:Car)=>`${c.brandName} ${c.description}`.toLocaleLowerCase().indexOf(filterText)!==-1):value
+    filter((c:Car)=>`${c.brandName} ${c.description} ${c.modelYear}`.toLocaleLowerCase().indexOf(filterText)!==-1):value
   }
 
 }
